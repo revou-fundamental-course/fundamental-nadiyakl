c786abe9d762377eafb5556a924642e3408a66c7
nadiyakl <nadiyakhali@gmail.com>
2024-07-04 14:53:22 +0700

    complete

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 
+/* banner slide */
+
+
+/* input name */
 let who = prompt("What we should call you?", "");
 document.getElementById("who").innerHTML = who
 
+
+/* form */
 function submitForm() {
     var nama = document.getElementById("nama").value;
     var emails = document.getElementById("emails").value;
     var judul = document.getElementById("judul").value;
     var pesan = document.getElementById("pesan").value;
+
+    console.log(nama);
+    console.log(emails);
+    console.log(judul);
+    console.log(pesan);
+
+/* output */
 
     var outputString = "Name: " + nama + "<br>" +
         "Email: " + emails + "<br>" +
@@ -14,4 +27,14 @@ function submitForm() {
         "Message: " + pesan;
 
     document.getElementById("output").innerHTML = outputString;
+
+/* validation */
+
+    if (nama != '' && emails != '' && judul != '' && pesan != '') {
+        alert('Your Message Sent!');
+    }
+
+    else {
+        alert('Fill the blank!');
+    }
 }
